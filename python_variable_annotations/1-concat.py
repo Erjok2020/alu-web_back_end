4733b2c20754e990075ac8246db1eddb32a4a280
Erjok2020 <e.mach@alustudent.com>
2024-01-14 18:45:50 +0300

    floor

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-'''Task 1's module.
-'''
 
 
 def concat(str1: str, str2: str) -> str:
---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+def floor(n: float) -> int:
+    '''returns the floor of a floating-point number.
+    '''
+    return int(n)
