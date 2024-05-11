07e29d8a29af7c3108bcfaa7d3710b5e867a19fb
Erjok2020 <e.mach@alustudent.com>
2024-05-11 22:04:45 +0300

    a type annotation that returns the floor of the float

---
+++ 2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Annotation to specify data types """
+
+import math
+
+
+def floor(n: float) -> int:
+    """ Returns the floor of the float """
+    return math.floor(n)
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -4,5 +4,5 @@
 
 
 def concat(str1: str, str2: str) -> str:
-    """ return concantenated string """
+    """ return  concatenated string """
     return str1 + str2
