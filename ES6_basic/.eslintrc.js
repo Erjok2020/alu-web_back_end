56450dad817c5f561eb432e011913e263ac4d8ab
Erjok2020 <e.mach@alustudent.com>
2024-07-20 00:34:16 +0300

    fix

---
+++ ES6_basic/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    // Add your custom rules here
+  },
+};
