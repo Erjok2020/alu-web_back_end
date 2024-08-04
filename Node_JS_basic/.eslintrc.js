a971ab9915b4d4372714a968340ed4d5b28d9ff9
Erjok2020 <e.mach@alustudent.com>
2024-08-05 00:27:05 +0300

    fix

--- Node_JS_basic/.eslintrc.js
+++ Node_JS_basic/.eslintrc.js
@@ -1,37 +1,42 @@
-module.exports = {
-  env: {
-    browser: false,
-    es6: true,
-    jest: true,
-  },
-  extends: [
-    'airbnb-base',
-    'plugin:jest/all',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['jest'],
-  rules: {
-    'max-classes-per-file': 'off',
-    'no-underscore-dangle': 'off',
-    'no-console': 'off',
-    'no-shadow': 'off',
-    'no-restricted-syntax': [
-      'error',
-      'LabeledStatement',
-      'WithStatement',
-    ],
-  },
-  overrides:[
-    {
-      files: ['*.js'],
-      excludedFiles: 'babel.config.js',
+const fs = require('fs');
+
+function countStudents(path) {
+  let content;
+
+  try {
+    content = fs.readFileSync(path, 'utf-8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  content = content.toString().trim().split('\n');
+
+  if (content.length <= 1) {
+    console.log('Number of students: 0');
+    return;
+  }
+
+  const students = content.slice(1).map((line) => line.split(','));
+  const NUMBER_OF_STUDENTS = students.length;
+  console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+
+  const fields = {};
+
+  for (const student of students) {
+    if (student.length < 4) continue; // Skip invalid entries
+
+    const field = student[3];
+    if (!fields[field]) {
+      fields[field] = [];
     }
-  ]
-};
+    fields[field].push(student[0]);
+  }
+
+  for (const [field, studentNames] of Object.entries(fields)) {
+    console.log(
+      `Number of students in ${field}: ${studentNames.length}. List: ${studentNames.join(', ')}`,
+    );
+  }
+}
+
+module.exports = countStudents;
