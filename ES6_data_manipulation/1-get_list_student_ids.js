c8317a0242cbb3e0dd4b99bd0679c435eb38052e
Erjok2020 <e.mach@alustudent.com>
2024-08-04 18:01:03 +0300

    fix

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,23 +1,9 @@
+export default function getListStudentIds(arr) {
+  let newArr = [];
+  if (arr instanceof Array) {
+    newArr = arr.map((item) => item.id);
+  }
 
-export default function getListStudents() {
-  const o1 = {
-    id: 1,
-    firstName: 'Guillaume',
-    location: 'San Francisco',
-  };
-
-  const o2 = {
-    id: 2,
-    firstName: 'James',
-    location: 'Columbia',
-  };
-
-  const o3 = {
-    id: 5,
-    firstName: 'Serena',
-    location: 'San Francisco',
-  };
-
-  return [o1, o2, o3];
+  return newArr;
 }
 
